961ee5c9e7ddf38db8e30bdbf914a0e6316f7346
Jesus.Santana <jfsantana77@gmail.com>
2023-10-12 16:14:06 -0400

    borrado de archivos no usados

--- add/footer.php
+++ add/footer.php
@@ -1,4 +1,4 @@
-<strong>Copyright &copy; 2023 <a href="https://adminlte.io">MCS </a>.</strong>
+<strong>Copyright &copy; 2023 <a href="#">MCS </a>.</strong>
 All rights reserved.
 <div class="float-right d-none d-sm-inline-block">
   <b>Version</b> 1.0
